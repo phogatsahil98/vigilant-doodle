94b198786c08ff41dc3f78cb2b56f5e90dcf18aa
phogatsahil98 <choudharynageshwarsingh242@gmail.com>
2025-09-18 20:44:24 +0530

    Finding Largest Element in an Array

--- 24_Array_Easy.cpp
+++ 24_Array_Easy.cpp
@@ -1,1 +1,43 @@
 //! To find out Largest element in an array
+/*
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    vector<int> arr{9, 87, 6, 56, 7};
+
+    sort(arr.begin(), arr.end());
+
+    // Printing whole array
+    // for(auto iter : arr){
+    //     cout<<iter<<" ";
+    // }
+    
+    //! Printing Largest Number 
+    int size = arr.size();
+    cout << arr[size-1];
+
+    return 0;
+}
+*/
+//! Optimization of above code 
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> arr{6, 5, 4, 9, 8, 7, 6, 4, 5, 6};
+
+    int maxElement = 1;
+
+    for (int i = 0; i < arr.size()-1; i++)
+    {
+        if (arr[i] > maxElement)
+        {
+            maxElement = arr[i];
+        }
+    }
+    
+    cout<<"Maximum Element : "<<maxElement;
+    return 0;
+}
