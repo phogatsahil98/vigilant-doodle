b1937b6bf550dccaf06240ad500efb13f2e04986
phogatsahil98 <choudharynageshwarsingh242@gmail.com>
2025-09-19 21:49:09 +0530

    Finding Largest Element in an Array without Sorting

--- 24_Array_Easy.cpp
+++ 24_Array_Easy.cpp
@@ -21,7 +21,8 @@ int main(){
     return 0;
 }
 */
-//! Optimization of above code 
+/*
+//! Optimization of above code
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -37,7 +38,57 @@ int main(){
             maxElement = arr[i];
         }
     }
-    
+
     cout<<"Maximum Element : "<<maxElement;
+    return 0;
+}
+*/
+
+//! Second Largest Element without Sorting and Optimal Approach
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    vector<int> arr{4, 5, 6, 7, 8, 7};
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    int smallest = INT_MAX;
+    int secondSmallest = INT_MAX;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        // Largest and Second Largest
+        if (arr[i] > largest)
+        {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondLargest && arr[i] != largest)
+        {
+            secondLargest = arr[i];
+        }
+
+        // Smallest and Second Smallest
+        if (arr[i] < smallest)
+        {
+            secondSmallest = smallest;
+            smallest = arr[i];
+        }
+        else if (arr[i] < secondSmallest && arr[i] != smallest)
+        {
+            secondSmallest = arr[i];
+        }
+    }
+
+    cout << "Largest: " << largest << endl;
+    cout << "Second Largest: " << secondLargest << endl;
+    cout << "Smallest: " << smallest << endl;
+    cout << "Second Smallest: " << secondSmallest << endl;
+
     return 0;
 }
